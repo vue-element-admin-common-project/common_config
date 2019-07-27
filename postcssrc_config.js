d3b8a8f3f4951699da71b2c66094b6d86f2fe664
chen1zee <chen2zee1@gmail.com>
2019-07-27 11:47:01 +0800

    [a]添加eslint

--- postcssrc_config.js
+++ postcssrc_config.js
@@ -3,8 +3,8 @@
  * */
 
 module.exports = {
-    "plugins": {
-        // to edit target browsers: use "browserslist" field in package.json
-        "autoprefixer": {}
-    }
+  "plugins": {
+    // to edit target browsers: use "browserslist" field in package.json
+    "autoprefixer": {}
+  }
 }
